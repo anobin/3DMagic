d19164e5fa240d4f4f1eeec44b4fb280143e972f
Andrew Keating <anobin@gmail.com>
2011-08-23 01:46:15 -0500

    Add Physics Collision Shapes

---
+++ include/CollisionShapes/CollisionShape.h
@@ -0,0 +1,97 @@
+/* 
+Copyright (c) 2011 Andrew Keating
+
+This file is part of 3DMagic.
+
+3DMagic is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+3DMagic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with 3DMagic.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/** Header file for CollisionShape base class
+ *
+ * @file CollisionShape.h
+ * @author Andrew Keating
+ */
+#ifndef MAGIC3D_COLLISION_SHAPE_H
+#define MAGIC3D_COLLISION_SHAPE_H
+
+
+#include "../Math/Vector3.h"
+
+// include bullet physics
+#include <btBulletDynamicsCommon.h>
+#include <btBulletCollisionCommon.h>
+
+
+namespace Magic3D
+{
+    
+class PhysicalBody;
+
+/** Base class for all collision shapes used in physics collison calculations.
+ * Note that the physical shape of an object can (and probably should) not match
+ * the graphical meshes exactly; as such bounding volumes of collision shapes
+ * should not be used graphical operations such as culling and intersection
+ * detection.
+ */
+class CollisionShape
+{
+protected:
+    friend class PhysicalBody;
+    
+    /// get the bullet physics collison shape
+    virtual btCollisionShape* getShape() = 0;
+
+    /// default constructor
+    inline CollisionShape() {}
+
+public:
+    
+    /// destructor
+    virtual ~CollisionShape();
+    
+    /** Get bounding sphere of collision shape. Does not necessarily (and probably wouldn't)
+     * represent the bounding sphere of the graphical meshes since they can be different
+     * from the physical collision shape (common for efficiency reasons).
+     */
+     inline void getBoundingSphere(Vector3* center, Scalar* radius)
+     {
+         btVector3 bt_center;
+         btScalar bt_radius;
+         this->getShape()->getBoundingSphere(bt_center, bt_radius);
+         center->setX(bt_center.getX());
+         center->setY(bt_center.getY());
+         center->setZ(bt_center.getZ());
+         (*radius) = (Scalar)bt_radius;
+     }
+
+
+
+
+};
+
+
+
+
+};
+
+
+
+
+
+
+#endif
+
+
+
+
---
+++ include/CollisionShapes/SphereCollisionShape.h
@@ -0,0 +1,74 @@
+/* 
+Copyright (c) 2011 Andrew Keating
+
+This file is part of 3DMagic.
+
+3DMagic is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+3DMagic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with 3DMagic.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/** Header file for SphereCollisionShape class
+ *
+ * @file SphereCollisionShape.h
+ * @author Andrew Keating
+ */
+#ifndef MAGIC3D_SPHERE_COLLISION_SHAPE_H
+#define MAGIC3D_SPHERE_COLLISION_SHAPE_H
+
+
+#include "CollisionShape.h"
+
+// include bullet physics
+#include <btBulletDynamicsCommon.h>
+#include <btBulletCollisionCommon.h>
+
+
+namespace Magic3D
+{
+
+/** Perfect sphere collision shape.
+ */
+class SphereCollisionShape : public CollisionShape
+{
+protected:
+    btSphereShape shape;
+    
+    /// get the bullet physics collison shape
+    virtual btCollisionShape* getShape();
+
+public:
+    /// default constructor
+    inline SphereCollisionShape(Scalar radius): shape((btScalar)radius) {}
+    
+    /// destructor
+    virtual ~SphereCollisionShape();
+
+
+
+};
+
+
+
+
+};
+
+
+
+
+
+
+#endif
+
+
+
+
